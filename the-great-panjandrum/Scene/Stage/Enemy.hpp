4e73d2698f68bc96b95c23a439afba9a40bba1c0
Oyakodon <oykdn@oykdn.com>
2018-01-26 23:54:45 +0900

    :construction: 敵のクラスを作った(未実装)

---
+++ the-great-panjandrum/Scene/Stage/Enemy.hpp
@@ -0,0 +1,120 @@
+#pragma once
+
+#include <Siv3D.hpp>
+
+/// <summary>
+/// 敵の種類
+/// </summary>
+enum class EnemyType
+{
+	Immovable,
+	Totter,
+	Barrage
+};
+
+/// <summary>
+/// 敵
+/// </summary>
+class Enemy
+{
+protected:
+
+	const Vec2 m_pos;
+
+	Vec2 m_playerPos;
+
+public:
+
+	Enemy(const Vec2& pos)
+		: m_pos(pos)
+	{ }
+
+	virtual ~Enemy() {}
+
+	virtual int getDamage() = 0;
+
+	void setPlayerPos(const Vec2& pos)
+	{
+		m_playerPos = pos;
+	}
+
+	virtual void update() = 0;
+
+	virtual void draw() const = 0;
+
+};
+
+/// <summary>
+/// Immovable, 動かないけど当たると痛い
+/// </summary>
+class Immovable : Enemy
+{
+
+public:
+
+	void update()
+	{
+
+	}
+
+	int getDamage()
+	{
+		return 0;
+	}
+
+	void draw() const
+	{
+		
+	}
+
+};
+
+/// <summary>
+/// Totter, よちよちブロックの上を左右に動く。当たると痛い。
+/// </summary>
+class Totter : Enemy
+{
+
+public:
+
+	void update()
+	{
+
+	}
+
+	int getDamage()
+	{
+		return 0;
+	}
+
+	void draw() const
+	{
+
+	}
+
+};
+
+/// <summary>
+/// Barrage, 空に浮かびながら弾を打ってくる。弾幕ってほどでもない。
+/// </summary>
+class Barrage : Enemy
+{
+
+public:
+
+	void update()
+	{
+
+	}
+
+	int getDamage()
+	{
+		return 0;
+	}
+
+	void draw() const
+	{
+
+	}
+
+};
